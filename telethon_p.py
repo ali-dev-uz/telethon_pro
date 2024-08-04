a936b1b5e824e334968162ed97521aa0b075801b
Ali <sevararuzieva2023@gmail.com>
2024-08-04 11:15:48 +0800

    update

--- telethon_p.py
+++ telethon_p.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 import pprint
 import re
 import sys
@@ -13,6 +14,11 @@ phone = '+447355739673'
 forward_to_username = 'MaestroSniperBot'
 buy_cost = 0.001
 
+logging.basicConfig(
+    filename='telethon_errors.log',
+    level=logging.ERROR,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
 
 client = TelegramClient('session_name', api_id, api_hash)
 sys.stdout.reconfigure(encoding='utf-8')
@@ -127,62 +133,137 @@ async def wait_for_reply_and_send_amount(entity, amount):
             break
 
 
-@client.on(events.NewMessage(from_users=5486942816))
+@client.on(events.NewMessage(from_users=5486942816, incoming=True))
 async def handle_message_new(event):
-    if event.message.reply_markup:
-        try:
+    if event.message.text in "Snipe amount set to":
+        pass
+    elif event.message.text in "❌ Buy":
+        pass
+    elif event.message.text in "⚡️Snipe activated":
+        pass
+    else:
+        if event.message.reply_markup:
+            try:
+                for row in event.message.reply_markup.rows:
+                    for button in row.buttons:
+                        if button.text == "⚙ Snipe Options":
+                            await event.message.click(4)
+                            # print(f"{button.text}")
+                            break
+                        elif button.text in '🎯 Snipe Now':
+                            await event.message.click(5)
+                            # print(f"{button.text}")
+                            break
+            except AttributeError:
+                pass
+
+
+@client.on(events.MessageEdited(from_users=5486942816, incoming=True))
+async def handle_message_edit(event):
+    forward_to = await client.get_entity(forward_to_username)
+
+    if event.message.text in "Snipe amount set to":
+        pass
+    else:
+        if event.message.reply_markup:
+            button_clicked = False
+            snipe_button = False
+            enable_on = False
             for row in event.message.reply_markup.rows:
                 for button in row.buttons:
-                    if button.text == "⚙ Snipe Options":
-                        await event.message.click(4)
-                        # print(f"{button.text}")
-                        break
-                    elif button.text == '🎯 Snipe Now':
-                        await event.message.click(5)
-                        # print(f"{button.text}")
-                        break
-        except AttributeError:
-            pass
-
-
-@client.on(events.MessageEdited(from_users=5486942816))
-async def handle_message_edit(event):
-    if event.message.reply_markup:
-        button_clicked = False
-        snipe_button = False
-        for row in event.message.reply_markup.rows:
-            for button in row.buttons:
-                if button.text == "❌ Snipe Disabled" and not button_clicked:
-                    snipe_button = True
-                else:
-                    if button.text == "✏️ Buy Amount" and snipe_button is True:
-                        await event.message.click(5)
-                        print(f"{button.text}-{buy_cost}")
-                        button_clicked = True
-                        break
-
-                    elif button.text != f"✏️ Buy Amount: {buy_cost} SOL" and snipe_button is True:
-                        await event.message.click(5)
-                        print(f"{button.text}-{buy_cost}")
-                        button_clicked = True
-                        break
+                    if button.text == "❌ Snipe Disabled" and not button_clicked:
+                        snipe_button = True
+                    elif button.text == "✅ Snipe Enabled" and not button_clicked:
+                        snipe_button = False
+                        enable_on = True
                     else:
-                        break
-            if button_clicked:
-                break
+                        if button.text == "✏️ Buy Amount" and snipe_button is True:
+                            await event.message.click(5)
+                            print(f"{button.text}-{buy_cost}")
+                            await client.send_message(forward_to, "clicked")
+                            button_clicked = True
+                            await event.message.click(7)  # Click the buy button
+                            # print(f"Clicked button: {second_menu_buttons[7]}")
+                            await wait_for_reply_and_send_amount(forward_to, f'{buy_cost}')
+                            break
+
+                        elif button.text != f"✏️ Buy Amount: {buy_cost} SOL" and snipe_button is True:
+                            await event.message.click(5)
+                            await client.send_message(forward_to, "clicked")
+                            print(f"{button.text}-{buy_cost}")
+                            await event.message.click(7)  # Click the buy button
+                            # print(f"Clicked button: {second_menu_buttons[7]}")
+                            await wait_for_reply_and_send_amount(forward_to, f'{buy_cost}')
+                            button_clicked = True
+                            break
+                        # elif button.text in "✏️ Buy Amount" and enable_on is True:
+                        #     await event.message.click(7)
+                        #     print(f"{button.text}-{buy_cost}")
+                        #     await wait_for_reply_and_send_amount(forward_to, f'{buy_cost}')
+                        #     button_clicked = True
+                        #     break
+                        else:
+                            break
+                if button_clicked:
+                    break
 
 
 
-@client.on(events.NewMessage(from_users=5486942816))
+@client.on(events.NewMessage(from_users=5486942816, pattern=r"❌ This token has launched already\."))
 async def handle_message_send(event):
     forward_to = await client.get_entity(forward_to_username)
     second_menu_buttons = []
-    if "❌ This token has launched already." in event.message.text:
-        msg_with_second_menu = await wait_for_buttons(forward_to, second_menu_buttons, 4)
-        if msg_with_second_menu:
-            await msg_with_second_menu.click(7)  # Click the buy button
-            # print(f"Clicked button: {second_menu_buttons[7]}")
-            await wait_for_reply_and_send_amount(forward_to, f'{buy_cost}')
+    if event.message.text in "Snipe amount set to":
+        pass
+    elif event.message.text in "❌ Buy":
+        pass
+    elif event.message.text in "⚡️Snipe activated":
+        pass
+    else:
+        if "❌ This token has launched already." in event.message.text:
+            msg_with_second_menu = await wait_for_buttons(forward_to, second_menu_buttons, 4)
+            if msg_with_second_menu:
+                await msg_with_second_menu.click(7)  # Click the buy button
+                # print(f"Clicked button: {second_menu_buttons[7]}")
+                await wait_for_reply_and_send_amount(forward_to, f'{buy_cost}')
+
+
+
+# @client.on(events.NewMessage(outgoing=True, pattern=r"clicked"))
+# async def handle_message_send_out(event):
+#     forward_to = await client.get_entity(forward_to_username)
+#     second_menu_buttons = []
+#     logging.error(f"Start", exc_info=True)
+#     print("Start")
+#     if event.message.text in "Snipe amount set to":
+#         pass
+#     else:
+#         msg_with_second_menu = await wait_for_buttons(forward_to, second_menu_buttons, 4)
+#         if msg_with_second_menu:
+#             await msg_with_second_menu.click(7)  # Click the buy button
+#             logging.error(f"Clicked button: {second_menu_buttons[7]}", exc_info=True)
+#             print(f"Clicked button: {second_menu_buttons[7]}")
+#             await wait_for_reply_and_send_amount(forward_to, f'{buy_cost}')
+
+
+
+#
+# @client.on(events.NewMessage(from_users=5486942816, pattern=r"Reply to this message with your desired buy amount (in SOL) when trading is available\."))
+# async def handle_message_send(event):
+#     forward_to = await client.get_entity(forward_to_username)
+#     if event.message.text in "Snipe amount set to":
+#         pass
+#     elif event.message.text in "❌ Buy":
+#         pass
+#     elif event.message.text in "⚡️Snipe activated":
+#         pass
+#     elif event.message.text in "Reply":
+#         await wait_for_reply_and_send_amount(forward_to, f'{buy_cost}')
+#         await client.send_message(forward_to, f'{buy_cost}', reply_to=event.message.id)
+#         print(f"Sent buy amount: {buy_cost}")
+#
+#     else:
+#         pass
 
 
 async def main():
